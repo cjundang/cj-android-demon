0a77c924a61a3f3e617fe213acca780d65cdc397
cjundang <cjundang@gmail.com>
2021-01-08 07:22:54 +0700

    Create json2.php

---
+++ json2.php
@@ -0,0 +1,18 @@
+<?php
+
+$data = array(
+    array(
+        "fname" => "Chanankorn",
+        "lname" => "Jandeng"
+    ),
+    array(
+        "fname" => "Chatchanan",
+        "lname" => "Jandeng"
+    ),
+    array(
+        "fname" => "Chaiyut",
+        "lname" => "Jandeng"
+    ),
+);
+
+echo json_encode($data);
