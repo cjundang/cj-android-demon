f6bb17672d4952c993d3e3a51184d372f18c0a05
cjundang <cjundang@gmail.com>
2021-01-22 04:25:16 +0700

    Create authen.php

---
+++ authen.php
@@ -0,0 +1,21 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+
+$x = $_POST['username'];
+$y = $_POST['password'];
+
+if($x == "cj" && $y == "1234"){
+    $data = array(
+        "code" => 0,
+        "status" => "success",
+        "fname" => "Chanankorn",
+        "lname" => "Jandeng"
+    );
+    echo json_encode($data);
+}else{
+    $data = array(
+        "code" => 1,
+        "status" => "fail"
+    );
+    echo json_encode($data);
+}
